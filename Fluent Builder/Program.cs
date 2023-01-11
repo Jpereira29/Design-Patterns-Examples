34a9c68a937f345a1931e9004ba6b5bf9f3db842
Jpereira29 <jorgepereira29ele@gmail.com>
2023-01-11 10:24:06 -0300

    fluent builder

---
+++ Fluent Builder/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace FluentBuilder1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var emailBuilder = new EmailBuilder();
+
+            var email = emailBuilder
+                       .To("microsoft@email.com")
+                       .From("jorgepereira29ele@gmail.com")
+                       .Subject("Micrsofot Build")
+                       .Body("Microsoft Build 2023\n\t11 a 27 de Janeiro 2023" +
+                                "\n\tNovidades Windows, Azure, Office e mais")
+                       .CriaEmail();
+
+            Console.WriteLine(email.ToString());
+            Console.ReadLine();
+        }
+    }
+}
